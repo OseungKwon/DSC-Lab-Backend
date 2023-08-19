2aab2a042cb311248cf6dbe8628f728280c8ec40
J-hoplin1 <jhoplin7259@gmail.com>
2023-08-19 15:39:33 +0900

    docs : add swagger config

---
+++ src/infrastructure/types/type.ts
@@ -0,0 +1,1 @@
+export type SwaggerTag = Record<string, string>;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { SwaggerModule } from '@nestjs/swagger';
+import swaggerConfig from '@infrastructure/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,9 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix('v1');
 
+  // Swagger document
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
