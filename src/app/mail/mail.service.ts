272de3fc1c45d53be76b6a1fb9c286205ac9c44f
J-hoplin1 <jhoplin7259@gmail.com>
2023-10-04 01:56:19 +0900

    Feat: Seperate Mail Module and add apply to Scheduler

---
+++ src/app/mail/mail.service.ts
@@ -0,0 +1,57 @@
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { MAIL_OPTION } from './mail.token';
+import { MailModuleOption, SendOption } from './type';
+import { SendMailOptions, createTransport } from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+@Injectable()
+export class MailService {
+  private logger = new Logger(MailService.name);
+  private transport: Mail;
+  private static transportConfig = {
+    service: 'gmail',
+    port: 587,
+    host: 'smtp.gmail.com',
+    secure: true,
+    requireTLS: true,
+  };
+
+  constructor(@Inject(MAIL_OPTION) private readonly option: MailModuleOption) {
+    if (!option?.gmailID || !option.gmailPW) {
+      throw new Error('ID and PW is required for MailModule');
+    }
+    this.transport = createTransport({
+      service: MailService.transportConfig.service,
+      port: MailService.transportConfig.port,
+      host: MailService.transportConfig.host,
+      secure: MailService.transportConfig.secure,
+      requireTLS: MailService.transportConfig.requireTLS,
+      auth: {
+        user: option.gmailID,
+        pass: option.gmailPW,
+      },
+    });
+  }
+
+  async sendMail(option: SendOption) {
+    try {
+      const sendOption: SendMailOptions = {
+        from: this.option.gmailID,
+        to: option.to,
+        subject: option.subject,
+        html: option.content,
+      };
+      await this.transport.sendMail(sendOption);
+    } catch (err) {
+      this.logger.error((err as Error)?.message);
+    }
+  }
+
+  async sendMailBatch(options: SendOption[]) {
+    await Promise.all(
+      options.map((option) => {
+        return this.sendMail(option);
+      }),
+    );
+  }
+}
---
+++ src/app/mail/mail.token.ts
@@ -0,0 +1,1 @@
+export const MAIL_OPTION = 'MAIL_OPTION';
